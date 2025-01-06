6175aa517e43cf5059a86346abb60f5e06d283b2
linuxct <linuxct@linuxct.space>
2025-01-07 00:15:56 +0100

    chore: Make script not return

--- hamclock/CHANGELOG.md
+++ hamclock/CHANGELOG.md
@@ -1,5 +1,9 @@
 <!-- https://developers.home-assistant.io/docs/add-ons/presentation#keeping-a-changelog -->
 
+## 0.4.5
+
+TODO 4.5
+
 ## 0.4.4
 
 TODO 4.4
--- hamclock/entrypoint.sh
+++ hamclock/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/with-contenv bashio
 
-echo "On entrypoint, start HamClock"
-/usr/local/bin/hamclock -o & disown
-echo "HamClock started, passing control to s6"
-/init # pass control to s6
+echo "Run init"
+/init & disown
+echo "Start HamClock"
+/usr/local/bin/hamclock -o
